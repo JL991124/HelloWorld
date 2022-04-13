e314833516af06e0f0c9531077c369ce91238e06
JL991124 <75029533+JL991124@users.noreply.github.com>
2022-04-13 16:07:53 +0800

    Add files via upload

---
+++ HelloWorld/HelloWorld/HelloWorld.swift
@@ -0,0 +1,12 @@
+//
+//  HelloWorld.swift
+//  HelloWorld
+//
+//  Created by jay.lam on 13/4/2022.
+//
+
+public class HelloWorld {
+    public static func helloWorld(name: String) {
+        print("Hello World, \(name)")
+    }
+}
